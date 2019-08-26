fa4ec2a7c504648e7b7f6cc9ad8a94ffa2b34d0b
Zachi Nachshon <zachi.nachshon@gmail.com>
2019-08-26 11:36:00 +0300

    [src] Added a quick-start script

---
+++ scripts/quick-start.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+ANCHOR_VERSION="v3.0.0"
+
+print_banner() {
+  echo -e "
+ █████╗ ███╗   ██╗ ██████╗██╗  ██╗ ██████╗ ██████╗
+██╔══██╗████╗  ██║██╔════╝██║  ██║██╔═══██╗██╔══██╗
+███████║██╔██╗ ██║██║     ███████║██║   ██║██████╔╝
+██╔══██║██║╚██╗██║██║     ██╔══██║██║   ██║██╔══██╗
+██║  ██║██║ ╚████║╚██████╗██║  ██║╚██████╔╝██║  ██║
+╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝ (${ANCHOR_VERSION})
+"
+}
+
+clone_anchor_dockerfiles_repo() {
+  echo -e "
+=======================================================================
+                          Clonning Repositories
+======================================================================="
+
+  echo -e "
+===========
+Repository: anchor-dockerfiles
+===========
+"
+
+  git clone "https://github.com/ZachiNachshon/anchor-dockerfiles.git" ${HOME}/.anchor/anchor-dockerfiles
+  echo -e "\n    Done."
+}
+
+set_dockerfiles_env_var() {
+
+  echo -e "
+=======================================================================
+                       Setting Environment Variables
+======================================================================="
+
+  # Copy to clipboard on macOS/Linux
+  local exportCmd="export DOCKER_FILES=${HOME}/.anchor/anchor-dockerfiles"
+  if [[ "$OSTYPE" == "linux"* ]]; then
+    xclip -selection "${exportCmd}"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "${exportCmd}" | pbcopy
+  fi
+
+  echo -e "\nRun the following command to link between anchor to DOCKER_FILES (paste from clipboard):
+
+  ${exportCmd}
+"
+}
+
+verify_pre_setup() {
+  read -p "Do you want to download anchor-dockerfiles to ${HOME}/.anchor? (y/n): " input
+  if [[ ${input} != "y" ]]; then
+    echo -e "\n    Nothing has changed.\n"
+    exit 0
+  fi
+}
+
+main() {
+  print_banner
+  verify_pre_setup
+  clone_anchor_dockerfiles_repo
+  set_dockerfiles_env_var
+}
+
+main "$@"
